efe47edb8019a699a5dd8c372ad267d99a5d9811
Jacob <kim4jc@mail.uc.edu>
2025-03-13 21:27:40 -0400

    implemented and styled details page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,7 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import DetailsPage from './components/DetailsPage/DetailsPage';
 function App() {
 
   return (
@@ -18,6 +18,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
