e2128ca10cefd18ce2ef7f0e3f1a6b1e5a695e03
Jacob <kim4jc@mail.uc.edu>
2025-03-14 22:05:46 -0400

    implemented backend in login component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,14 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './LoginPage.css';
+//Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import { urlConfig } from '../../config';
+//Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from '../../context.AuthContext';
+//Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
 
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    //Task 4: Include a state for incorrect password.
+    const [incorrect, setIncorrect] = useState('');
+    //Task 5: Create a local variable for `navigate`,`bearerToken`   and `setIsLoggedIn`.
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    //Task 6. If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+        if(sessionStorage.getItem('auth-token')){
+            navigate('/app')
+        }
+    }, [navigate])
+
     // insert code here to create handleLogin function and include console.log
     const handleLogin = async () => {
+        e.preventDefault();
         console.log("Inside handleLogin");
+        try{
+            //first task
+            const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+                //Task 7: Set method
+                method: 'POST',
+                //Task 8: Set headers
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+                },
+                //Task 9: Set body to send user details
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                })
+            });
+            // Task 1: Access data coming from fetch API
+            const json = await res.json();
+            console.log('Json',json);
+                if (json.authtoken) {
+                    // Task 2: Set user details
+                    sessionStorage.setItem('auth-token', json.authtoken);
+                    sessionStorage.setItem('name', json.userName);
+                    sessionStorage.setItem('email', json.userEmail);
+                    // Task 3: Set the user's state to log in using the `useAppContext`.
+                    setIsLoggedIn(true);
+                    // Task 4: Navigate to the MainPage after logging in.
+                    navigate('/app');
+                }
+                else{ 
+                    // Task 5: Clear input and set an error message if the password is incorrect
+                    document.getElementById("email").value="";
+                    document.getElementById("password").value="";
+                    setIncorrect("Wrong password. Try again.");
+                    setTimeout(() => {
+                      setIncorrect("");
+                    }, 2000);
+                }
+        }catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
     }
 
         return (
@@ -44,6 +106,8 @@ function LoginPage() {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
+
+                <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
             </div>
           {/* insert code here to create a button that performs the `handleLogin` function on click */}
           <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
